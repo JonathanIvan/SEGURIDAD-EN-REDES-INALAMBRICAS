1409a93a83bca023f7ce753fd6923ef5b27e1337
JonathanIvan <jirp.djnavi@gmail.com>
2015-05-15 23:32:00 -0500

    ventana del router Arris TG862A

---
+++ SeguridadRedInalambrica/SeguridadRedInalambrica/VentanaArrisTG862A.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SeguridadRedInalambrica
+{
+    public partial class VentanaArrisTG862A : Form
+    {
+        public VentanaArrisTG862A()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCambiar_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnRegresar_Click(object sender, EventArgs e)
+        {
+            VentanaSeleccion seleccion = new VentanaSeleccion();
+            seleccion.Show();
+            this.SetVisibleCore(false);
+        }
+
+        private void VentanaArrisTG862A_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
