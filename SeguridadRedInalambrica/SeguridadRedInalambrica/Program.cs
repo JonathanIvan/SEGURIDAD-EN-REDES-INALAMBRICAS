765cc752be57070435bee91d4791a92c07f70252
JonathanIvan <jirp.djnavi@gmail.com>
2015-05-15 23:29:40 -0500

    inicio

---
+++ SeguridadRedInalambrica/SeguridadRedInalambrica/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SeguridadRedInalambrica
+{
+    static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            PantallaInicio pi = new PantallaInicio();
+            if (pi.ShowDialog ()== DialogResult.OK)
+            {
+                Application.Run(new VentanaSeleccion());
+            }
+        }
+    }
+}
