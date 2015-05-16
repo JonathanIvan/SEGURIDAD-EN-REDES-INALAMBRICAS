a676a3f58be9bb1a90a76039d9f9e7261e9ec381
JonathanIvan <jirp.djnavi@gmail.com>
2015-05-15 23:31:21 -0500

    ventana de router Huawei WS319

---
+++ SeguridadRedInalambrica/SeguridadRedInalambrica/VenatnaHuaweiWS319.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace SeguridadRedInalambrica
+{
+    partial class VenatnaHuaweiWS319
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(VenatnaHuaweiWS319));
+            this.btnCambiar = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTipoSeguridad = new System.Windows.Forms.ComboBox();
+            this.txtContraRed = new System.Windows.Forms.TextBox();
+            this.txtNombreRed = new System.Windows.Forms.TextBox();
+            this.btnRegresar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnCambiar
+            // 
+            this.btnCambiar.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCambiar.Image = ((System.Drawing.Image)(resources.GetObject("btnCambiar.Image")));
+            this.btnCambiar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCambiar.Location = new System.Drawing.Point(234, 250);
+            this.btnCambiar.Name = "btnCambiar";
+            this.btnCambiar.Size = new System.Drawing.Size(91, 33);
+            this.btnCambiar.TabIndex = 15;
+            this.btnCambiar.Text = "Cambiar";
+            this.btnCambiar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnCambiar.UseVisualStyleBackColor = true;
+            this.btnCambiar.Click += new System.EventHandler(this.btnCambiar_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(178, 178);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(112, 15);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Tipo de Seguridad:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(178, 125);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 15);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Contraseña:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(178, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(115, 15);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Nombre Red (SSD):";
+            // 
+            // txtTipoSeguridad
+            // 
+            this.txtTipoSeguridad.FormattingEnabled = true;
+            this.txtTipoSeguridad.Location = new System.Drawing.Point(178, 204);
+            this.txtTipoSeguridad.Name = "txtTipoSeguridad";
+            this.txtTipoSeguridad.Size = new System.Drawing.Size(169, 21);
+            this.txtTipoSeguridad.TabIndex = 11;
+            // 
+            // txtContraRed
+            // 
+            this.txtContraRed.Location = new System.Drawing.Point(178, 151);
+            this.txtContraRed.Name = "txtContraRed";
+            this.txtContraRed.Size = new System.Drawing.Size(169, 20);
+            this.txtContraRed.TabIndex = 10;
+            // 
+            // txtNombreRed
+            // 
+            this.txtNombreRed.Location = new System.Drawing.Point(178, 92);
+            this.txtNombreRed.Name = "txtNombreRed";
+            this.txtNombreRed.Size = new System.Drawing.Size(169, 20);
+            this.txtNombreRed.TabIndex = 9;
+            // 
+            // btnRegresar
+            // 
+            this.btnRegresar.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegresar.Image = ((System.Drawing.Image)(resources.GetObject("btnRegresar.Image")));
+            this.btnRegresar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnRegresar.Location = new System.Drawing.Point(396, 26);
+            this.btnRegresar.Name = "btnRegresar";
+            this.btnRegresar.Size = new System.Drawing.Size(92, 27);
+            this.btnRegresar.TabIndex = 16;
+            this.btnRegresar.Text = "Regresar";
+            this.btnRegresar.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnRegresar.UseVisualStyleBackColor = true;
+            this.btnRegresar.Click += new System.EventHandler(this.btnRegresar_Click);
+            // 
+            // VenatnaHuaweiWS319
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(524, 343);
+            this.Controls.Add(this.btnRegresar);
+            this.Controls.Add(this.btnCambiar);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtTipoSeguridad);
+            this.Controls.Add(this.txtContraRed);
+            this.Controls.Add(this.txtNombreRed);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "VenatnaHuaweiWS319";
+            this.Text = "VenatnaHuaweiWS319";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.VentanaHuaweiWS319);
+            this.Load += new System.EventHandler(this.VenatnaHuaweiWS319_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCambiar;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox txtTipoSeguridad;
+        private System.Windows.Forms.TextBox txtContraRed;
+        private System.Windows.Forms.TextBox txtNombreRed;
+        private System.Windows.Forms.Button btnRegresar;
+    }
+}
---
+++ SeguridadRedInalambrica/SeguridadRedInalambrica/VenatnaHuaweiWS319.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SeguridadRedInalambrica
+{
+    public partial class VenatnaHuaweiWS319 : Form
+    {
+        public VenatnaHuaweiWS319()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRegresar_Click(object sender, EventArgs e)
+        {
+            VentanaSeleccion seleccion = new VentanaSeleccion();
+            seleccion.Show();
+            this.SetVisibleCore(false);
+        }
+
+        private void btnCambiar_Click(object sender, EventArgs e)
+        {
+
+        }
+       
+        private void VenatnaHuaweiWS319_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void VentanaHuaweiWS319(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
