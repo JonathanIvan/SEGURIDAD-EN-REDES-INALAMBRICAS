0dd72fb9dcd70b46d1df8761b2e6fd50faf9fb80
JonathanIvan <jirp.djnavi@gmail.com>
2015-05-16 00:10:31 -0500

    modificacion 1.1

--- SeguridadRedInalambrica/SeguridadRedInalambrica/VenatnaHuaweiWS319.cs
+++ SeguridadRedInalambrica/SeguridadRedInalambrica/VenatnaHuaweiWS319.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -26,6 +28,134 @@ namespace SeguridadRedInalambrica
 
         private void btnCambiar_Click(object sender, EventArgs e)
         {
+            CookieContainer Login = new CookieContainer();
+            //ServicePointManager.ServerCertificateValidationCallback = new System.Net.Security.RemoteCertificateValidationCallback(AcceptAllCertifications);
+            HttpWebRequest peticion = (HttpWebRequest)WebRequest.Create("http://192.168.3.1/html/index.html");
+            peticion.Method = "GET";
+            peticion.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8";
+            peticion.KeepAlive = true;
+            peticion.CookieContainer = Login;
+            peticion.Host = "192.168.3.1";
+            peticion.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko)Chrome/36.0.1985.143 Safari/537.36";
+
+
+            HttpWebResponse respuestainicio = (HttpWebResponse)peticion.GetResponse();
+            StreamReader lectorinicio = new StreamReader(respuestainicio.GetResponseStream());
+            string html3 = lectorinicio.ReadToEnd();
+
+            string[] fuentes = html3.Split(new string[] { "<meta name=\"csrf_param\" content=\"", "\"/>" }, StringSplitOptions.None);
+
+            string param = fuentes[1].Trim();
+
+
+
+            string[] fuentes2 = html3.Split(new string[] { "<meta name=\"csrf_token\" content=\"", "\"/>" }, StringSplitOptions.None);
+
+            string token = fuentes2[2].Trim();
+
+
+
+            CookieContainer Login2 = new CookieContainer();
+            string json = "{\"csrf\":{\"csrf_param\":\"" + param + "\",\"csrf_token\":\"" + token + "\"},\"data\":{\"UserName\":\"admin\",\"Password\":\"OGM2OTc2ZTViNTQxMDQxNWJkZTkwOGJkNGRlZTE1ZGZiMTY3YTljODczZmM0YmI4YTgxZjZmMmFiNDQ4YTkxOA==\"}}";
+
+            byte[] byteArray = Encoding.UTF8.GetBytes(json);
+
+            HttpWebRequest peticionLogin = (HttpWebRequest)WebRequest.Create("http://192.168.3.1/api/system/user_login");
+            peticionLogin.ProtocolVersion = HttpVersion.Version11;
+            peticionLogin.Method = "POST";
+            peticionLogin.Accept = "application/json, text/javascript, */*; q=0.01";
+            peticionLogin.Headers.Add("X-Requested-With", "XMLHttpRequest");
+            peticionLogin.AllowAutoRedirect = true;
+            peticionLogin.CookieContainer = Login;
+            peticionLogin.ContentLength = 234;
+            peticionLogin.ContentType = " application/json;charset=UTF-8";
+            peticionLogin.Host = "192.168.3.1";
+            peticionLogin.Referer = "http://192.168.3.1/html/index.html";
+            peticionLogin.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.143 Safari/537.36";
+
+            StreamWriter sw = new StreamWriter(peticionLogin.GetRequestStream());
+
+            sw.Write(json);
+            sw.Flush();
+            sw.Close();
+
+            HttpWebResponse respuestaLogin = (HttpWebResponse)peticionLogin.GetResponse();
+            StreamReader lectorLogin = new StreamReader(respuestaLogin.GetResponseStream());
+            string html = lectorLogin.ReadToEnd();
+
+
+
+
+            HttpWebRequest peticionWizard = (HttpWebRequest)WebRequest.Create("http://192.168.3.1/html/wizard/wizard.html");
+
+            peticionWizard.Method = "GET";
+            peticionWizard.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8";
+            peticionWizard.KeepAlive = true;
+            peticionWizard.CookieContainer = Login;
+            peticionWizard.Host = "192.168.3.1";
+            peticionWizard.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.143 Safari/537.36";
+            HttpWebResponse respuestaWinzard = (HttpWebResponse)peticionWizard.GetResponse();
+            StreamReader lectorWinzard = new StreamReader(respuestaWinzard.GetResponseStream());
+            string html1 = lectorWinzard.ReadToEnd();
+
+            HttpWebRequest peticionAdvanceLan = (HttpWebRequest)WebRequest.Create("http://192.168.3.1/html/advance.html#landevices");
+
+            peticionAdvanceLan.Method = "GET";
+            peticionAdvanceLan.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8";
+            peticionAdvanceLan.KeepAlive = true;
+            peticionAdvanceLan.CookieContainer = Login;
+            peticionAdvanceLan.Host = "192.168.3.1";
+            peticionAdvanceLan.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.143 Safari/537.36";
+            HttpWebResponse respuestaAdvanceLan = (HttpWebResponse)peticionAdvanceLan.GetResponse();
+            StreamReader lectorAdvanceLan = new StreamReader(respuestaAdvanceLan.GetResponseStream());
+            string htmlAdvance = lectorAdvanceLan.ReadToEnd();
+            string[] fuentesAdvance = htmlAdvance.Split(new string[] { "<meta name=\"csrf_param\" content=\"", "\"/>" }, StringSplitOptions.None);
+
+            string paramAdvance = fuentesAdvance[1].Trim();
+
+
+
+            string[] fuentesAdvance2 = htmlAdvance.Split(new string[] { "<meta name=\"csrf_token\" content=\"", "\"/>" }, StringSplitOptions.None);
+
+            string tokeAdvance = fuentesAdvance2[2].Trim();
+
+
+            HttpWebRequest peticionWifisync = (HttpWebRequest)WebRequest.Create("http://192.168.3.1/api/ntwk/wlanwifisync");
+
+            peticionWifisync.Method = "GET";
+            peticionWifisync.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8";
+            peticionWifisync.KeepAlive = true;
+            peticionWifisync.CookieContainer = Login;
+            peticionWifisync.Host = "192.168.3.1";
+            peticionWifisync.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.143 Safari/537.36";
+            HttpWebResponse respuestaWifisync = (HttpWebResponse)peticionWifisync.GetResponse();
+            StreamReader lectorWifisync = new StreamReader(respuestaWifisync.GetResponseStream());
+            string htmlWifisync = lectorWifisync.ReadToEnd();
+
+            string json5 = "{\"csrf\":{\"csrf_param\":\"" + paramAdvance + "\",\"csrf_token\":\"" + tokeAdvance + "\"},\"action\":\"AllSsidSettings\",\"data\":[{\"ID\":\"InternetGatewayDevice.LANDevice.1.WLANConfiguration.1.\",\"ssid\":\"" + txtNombreRed.Text + "\",\"ssidenable\":true,\"isolate\":false,\"advertisement\":1,\"beacontype\":\"WPAand11i\",\"wpaencmode\":\"TKIP+AES\",\"wpakey\":\"" + txtContraRed.Text + "\"}]}";
+            byte[] byteArray2 = Encoding.UTF8.GetBytes(json5);
+            
+            HttpWebRequest peticionWlan = (HttpWebRequest)WebRequest.Create("http://192.168.3.1/api/ntwk/WlanBasic");
+
+            peticionWlan.Accept = "application/json, text/javascript, */*; q=0.01";
+            peticionWlan.ContentLength = byteArray2.Length;
+            peticionWlan.Method = "POST";
+            peticionWlan.Headers.Add("X-Requested-With", "XMLHttpRequest");
+
+            peticionWlan.ContentType = " application/json;charset=UTF-8";
+            peticionWlan.KeepAlive = true;
+            peticionWlan.CookieContainer = Login;
+            peticionWlan.Host = "192.168.3.1";
+            peticionWlan.Referer = "http://192.168.3.1/html/advance.html";
+            peticionWlan.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko)Chrome/36.0.1985.143 Safari/537.36";
+            StreamWriter sw2 = new StreamWriter(peticionWlan.GetRequestStream());
+
+            sw2.Write(json5, 0, byteArray2.Length);
+            sw2.Flush();
+            sw2.Close();
+          
+            HttpWebResponse respuestaWlan = (HttpWebResponse)peticionWlan.GetResponse();
+            StreamReader lectorWlan = new StreamReader(respuestaWlan.GetResponseStream());
 
         }
        
--- SeguridadRedInalambrica/SeguridadRedInalambrica/VentanaCiscoDCP2420.cs
+++ SeguridadRedInalambrica/SeguridadRedInalambrica/VentanaCiscoDCP2420.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Net;
 
 namespace SeguridadRedInalambrica
 {
@@ -15,6 +17,7 @@ namespace SeguridadRedInalambrica
         public VentanaCiscoDCP2420()
         {
             InitializeComponent();
+            MostrarDatos();
         }
 
         private void btnRegresar_Click(object sender, EventArgs e)
@@ -24,9 +27,208 @@ namespace SeguridadRedInalambrica
             this.SetVisibleCore(false);
         }
 
+        public void Desencriptar() {
+            CookieContainer login2 = new CookieContainer();
+            string postData = "username_login=cisco&password_login=cisco&LanguageSelect=en&Language_Submit=0&login=Log+In";
+            byte[] byteArray = Encoding.UTF8.GetBytes(postData);
+
+
+            HttpWebRequest peticionlogin = (HttpWebRequest)WebRequest.Create("http://192.168.0.1/goform/Docsis_system");
+            peticionlogin.ProtocolVersion = HttpVersion.Version11;
+            peticionlogin.Method = "POST";
+            peticionlogin.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8";
+            peticionlogin.ContentLength = 90;
+            peticionlogin.ContentType = "application/x-www-form-urlencoded";
+            peticionlogin.CookieContainer = login2;
+            peticionlogin.Host = "192.168.0.1";
+            peticionlogin.Referer = "192.168.0.1/Docsis_system.asp";
+            peticionlogin.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.143 Safari/537.36";
+
+
+
+            StreamWriter sw = new StreamWriter(peticionlogin.GetRequestStream());
+            sw.Write(postData, 0, byteArray.Length);
+            sw.Close();
+
+            HttpWebResponse respuestaLogin = (HttpWebResponse)peticionlogin.GetResponse();
+            StreamReader lectorLogin = new StreamReader(respuestaLogin.GetResponseStream());
+            string html = lectorLogin.ReadToEnd();
+
+            HttpWebRequest peticion = (HttpWebRequest)WebRequest.Create("http://192.168.0.1/Quick_setup.asp");
+            peticion.Method = "GET";
+            peticion.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8";
+            peticion.KeepAlive = true;
+            peticion.CookieContainer = login2;
+            peticion.Host = "192.168.0.1";
+            peticion.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko)Chrome/36.0.1985.143 Safari/537.36";
+
+
+            HttpWebResponse respuesta = (HttpWebResponse)peticion.GetResponse();
+            StreamReader lector = new StreamReader(respuesta.GetResponseStream());
+            string html1 = lector.ReadToEnd();
+
+            string postData2 = "Password=&PasswordReEnter=&setup_wifi_enable=enable&security_mode=psk2_mixed&radius_ip_1=0&" +
+            "radius_ip_2=0&radius_ip_3=0&radius_ip_4=0&keysize=64&tx_key=" +
+            "1&wl5g_setup_wifi_enable=enable&qs_h_network_band_5G=2&wl5g_ssid=" + txtNombreRed.Text
+            + "&wl5g_security_mode=psk2&wl5g_wpa_enc=aes&wl5g_wpa_psk_key=" + txtContraRed.Text
+            + "g_UnhiddenPresharedKey=0x01&wl5g_radius_ip_1=0&wl5g_radius_ip_2"
+            + "=0&wl5g_radius_ip_3=0&wl5g_radius_ip_4=0&wl5g_keysize=64&wl5g_tx_key="
+            + "1&save=Save+Settings&h_wl5g_setup_wifi_enable=enable&h_wl5g_security_mode=psk2&h_wl5g_wpa_enc" +
+            "=aes&qs_wds_setting=disable&wl5g_qs_wds_setting=disable&UserId=";
+            byte[] byteArray2 = Encoding.UTF8.GetBytes(postData2);
+
+
+            HttpWebRequest peticionLoginRed = (HttpWebRequest)WebRequest.Create("http://192.168.0.1/goform/Quick_setup");
+            peticionLoginRed.ProtocolVersion = HttpVersion.Version11;
+            peticionLoginRed.Method = "POST";
+            peticionLoginRed.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8";
+            peticionLoginRed.ContentLength = byteArray2.Length;
+            peticionLoginRed.ContentType = "application/x-www-form-urlencoded";
+            peticionLoginRed.CookieContainer = login2;
+            peticionLoginRed.Host = "192.168.0.1";
+            peticionLoginRed.Referer = "192.168.0.1/Quick_setup.asp";
+            peticionLoginRed.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.143 Safari/537.36";
+
+
+
+            StreamWriter Escrtitura = new StreamWriter(peticionLoginRed.GetRequestStream());
+            Escrtitura.Write(postData2, 0, byteArray2.Length);
+            Escrtitura.Close();
+
+            HttpWebResponse respuestaLoginRed = (HttpWebResponse)peticionlogin.GetResponse();
+            StreamReader lectorLoginRed = new StreamReader(respuestaLoginRed.GetResponseStream());
+            string html3 = lectorLogin.ReadToEnd();
+        
+        
+        }
+
+        public void MostrarDatos()
+        {
+            CookieContainer login = new CookieContainer();
+            string postData = "username_login=cisco&password_login=cisco&LanguageSelect=en&Language_Submit=0&login=Log+In";
+            byte[] byteArray = Encoding.UTF8.GetBytes(postData);
+
+
+            HttpWebRequest peticionlogin = (HttpWebRequest)WebRequest.Create("http://192.168.0.1/goform/Docsis_system");
+            peticionlogin.ProtocolVersion = HttpVersion.Version11;
+            peticionlogin.Method = "POST";
+            peticionlogin.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8";
+            peticionlogin.ContentLength = 90;
+            peticionlogin.ContentType = "application/x-www-form-urlencoded";
+            peticionlogin.CookieContainer = login;
+            peticionlogin.Host = "192.168.0.1";
+            peticionlogin.Referer = "192.168.0.1/Docsis_system.asp";
+            peticionlogin.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.143 Safari/537.36";
+
+
+
+            StreamWriter sw = new StreamWriter(peticionlogin.GetRequestStream());
+            sw.Write(postData, 0, byteArray.Length);
+            sw.Close();
+
+            HttpWebResponse respuestaLogin = (HttpWebResponse)peticionlogin.GetResponse();
+            StreamReader lectorLogin = new StreamReader(respuestaLogin.GetResponseStream());
+            string html = lectorLogin.ReadToEnd();
+
+
+            HttpWebRequest peticion = (HttpWebRequest)WebRequest.Create("http://192.168.0.1/Quick_setup.asp");
+            peticion.Method = "GET";
+            peticion.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8";
+            peticion.KeepAlive = true;
+            peticion.CookieContainer = login;
+            peticion.Host = "192.168.0.1";
+            peticion.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko)Chrome/36.0.1985.143 Safari/537.36";
+
+
+            HttpWebResponse respuesta = (HttpWebResponse)peticion.GetResponse();
+            StreamReader lector = new StreamReader(respuesta.GetResponseStream());
+            string html1 = lector.ReadToEnd();
+
+            string[] fuentes1 = html1.Split(new string[] { "<input type=\"text\" maxlength=\"32\" name=\"wl5g_ssid\" id=\"wl5g_ssid\" value=\"", "\" />" }, StringSplitOptions.None);
+
+
+            string SSDI = fuentes1[21].Trim();
+
+            txtNombreRed.Text = "" + SSDI;
+            string[] fuentes2 = html1.Split(new string[] { "<input type=\"text\" class=\"num\" id=\"wl5g_wpa_psk_key\" name=\"wl5g_wpa_psk_key\" size=\"32\" maxlength=\"64\" value=\"", "\" onKeyDown=\"keyCheck(this)\"  onSelect=\"textSelected=true\" />" }, StringSplitOptions.None);
+
+
+            string Password = fuentes2[2].Trim();
+            txtContraRed.Text = "" + Password;
+        }
+
         private void btnCambiar_Click(object sender, EventArgs e)
         {
+            CookieContainer login2 = new CookieContainer();
+            string postData = "username_login=cisco&password_login=cisco&LanguageSelect=en&Language_Submit=0&login=Log+In";
+            byte[] byteArray = Encoding.UTF8.GetBytes(postData);
 
+
+            HttpWebRequest peticionlogin = (HttpWebRequest)WebRequest.Create("http://192.168.0.1/goform/Docsis_system");
+            peticionlogin.ProtocolVersion = HttpVersion.Version11;
+            peticionlogin.Method = "POST";
+            peticionlogin.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8";
+            peticionlogin.ContentLength = 90;
+            peticionlogin.ContentType = "application/x-www-form-urlencoded";
+            peticionlogin.CookieContainer = login2;
+            peticionlogin.Host = "192.168.0.1";
+            peticionlogin.Referer = "192.168.0.1/Docsis_system.asp";
+            peticionlogin.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.143 Safari/537.36";
+
+
+
+            StreamWriter sw = new StreamWriter(peticionlogin.GetRequestStream());
+            sw.Write(postData, 0, byteArray.Length);
+            sw.Close();
+
+            HttpWebResponse respuestaLogin = (HttpWebResponse)peticionlogin.GetResponse();
+            StreamReader lectorLogin = new StreamReader(respuestaLogin.GetResponseStream());
+            string html = lectorLogin.ReadToEnd();
+
+            HttpWebRequest peticion = (HttpWebRequest)WebRequest.Create("http://192.168.0.1/Quick_setup.asp");
+            peticion.Method = "GET";
+            peticion.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8";
+            peticion.KeepAlive = true;
+            peticion.CookieContainer = login2;
+            peticion.Host = "192.168.0.1";
+            peticion.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko)Chrome/36.0.1985.143 Safari/537.36";
+
+
+            HttpWebResponse respuesta = (HttpWebResponse)peticion.GetResponse();
+            StreamReader lector = new StreamReader(respuesta.GetResponseStream());
+            string html1 = lector.ReadToEnd();
+
+            string postData2 = "Password=&PasswordReEnter=&setup_wifi_enable=enable&security_mode=psk2_mixed&radius_ip_1=0&" +
+            "radius_ip_2=0&radius_ip_3=0&radius_ip_4=0&keysize=64&tx_key=" +
+            "1&wl5g_setup_wifi_enable=enable&qs_h_network_band_5G=2&wl5g_ssid=" + txtNombreRed.Text
+            + "&wl5g_security_mode=psk2&wl5g_wpa_enc=aes&wl5g_wpa_psk_key=" + txtContraRed.Text
+            + "g_UnhiddenPresharedKey=0x01&wl5g_radius_ip_1=0&wl5g_radius_ip_2"
+            + "=0&wl5g_radius_ip_3=0&wl5g_radius_ip_4=0&wl5g_keysize=64&wl5g_tx_key="
+            + "1&save=Save+Settings&h_wl5g_setup_wifi_enable=enable&h_wl5g_security_mode=psk2&h_wl5g_wpa_enc" +
+            "=aes&qs_wds_setting=disable&wl5g_qs_wds_setting=disable&UserId=";
+            byte[] byteArray2 = Encoding.UTF8.GetBytes(postData2);
+
+
+            HttpWebRequest peticionLoginRed = (HttpWebRequest)WebRequest.Create("http://192.168.0.1/goform/Quick_setup");
+            peticionLoginRed.ProtocolVersion = HttpVersion.Version11;
+            peticionLoginRed.Method = "POST";
+            peticionLoginRed.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8";
+            peticionLoginRed.ContentLength = byteArray2.Length;
+            peticionLoginRed.ContentType = "application/x-www-form-urlencoded";
+            peticionLoginRed.CookieContainer = login2;
+            peticionLoginRed.Host = "192.168.0.1";
+            peticionLoginRed.Referer = "192.168.0.1/Quick_setup.asp";
+            peticionLoginRed.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.143 Safari/537.36";
+
+
+
+            StreamWriter Escrtitura = new StreamWriter(peticionLoginRed.GetRequestStream());
+            Escrtitura.Write(postData2, 0, byteArray2.Length);
+            Escrtitura.Close();
+
+            HttpWebResponse respuestaLoginRed = (HttpWebResponse)peticionlogin.GetResponse();
+            StreamReader lectorLoginRed = new StreamReader(respuestaLoginRed.GetResponseStream());
+            string html3 = lectorLogin.ReadToEnd();
         }
 
         private void VentanaCiscoDCP2420_FormClosed(object sender, FormClosedEventArgs e)
